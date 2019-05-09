e8603a544884a8baf7fc118ff35909d93ebeaec0
Jon Thomas Edward Williams <jtew396@gmail.com>
2019-05-09 21:30:22 +0000

    Practicing Integration

--- application.py
+++ application.py
@@ -13,3 +13,21 @@ socketio = SocketIO(app)
 def index():
     return render_template("index.html")
 
+@app.route("/convert", methods=["POST"])
+def convert():
+
+    # Query for currency exchange rate
+    currency = request.form.get("currency")
+    res = request.get("https://api.fixer.io/latest", params={
+        "base": "USD", "symbols": currency})
+
+    # Make sure request succeeded
+    if res.status_code != 200:
+        return jsonify({"success": False})
+
+    # Make sure currency is in response
+    data = res.json()
+    if currency not in data["rates"]:
+        return jsonify({"success": False})
+
+    return jsonify({"success": True, "rate": data["rates"][currency]})
