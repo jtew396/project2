f184d594e34bf2796a0a31c5340c025b04fe8e23
jtew396 <jtew396@gmail.com>
2019-05-14 12:49:26 -0500

    Improving Front End

--- application.py
+++ application.py
@@ -1,5 +1,4 @@
 import os
-#import requests
 
 from flask import Flask, jsonify, render_template, request
 from flask_socketio import SocketIO, emit
@@ -9,12 +8,28 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@socketio.on("register")
-def register(data):
+@app.route("/register", methods=["POST"])
+def register():
+
+    # Query for the username
+    username = request.form.get("username-create")
+    print(username)
+
+    # Make sure the username was successfully passed
+    if not username:
+        return jsonify({"success": False})
+
+
+
+# @app.route("/login", methods=["POST"])
+# def register():
+
+#    return jsonify({"success": True, "username": username})
 
 
 
--- static/index.js
+++ static/index.js
@@ -4,7 +4,46 @@ const template = Handlebars.compile("<li>New Channel</li>");
 // Storing a list of channels
 const channels = []
 
+// Storing a list of users
+const users = []
+
 document.addEventListener('DOMContentLoaded', () => {
+
+    // Hide the inner HTML of the frame before logging the user in
+    document.querySelector('#frame').style.visibility = 'hidden';
+
+    // This is adapted from the currency app, needs to be adjusted for register & login
+    document.querySelector('#new-user').onsubmit = () => {
+
+        // Initialize new request
+        const request = new XMLHttpRequest();
+        const username = document.querySelector('#username-create').value;
+        request.open('POST', '/register');
+
+        // Callback function for when request completes
+        request.onload = () => {
+
+            // Extract JSON data from requests
+            const data = JSON.parse(request.responseText);
+
+            // Update the webpage
+            if (data.success) {
+                const contents = `${data.username}`;
+                document.querySelector('#result').innerHTML = contents;
+            }
+            else {
+                document.querySelector('#result').innerHTML = 'Username already exists.';
+            }
+        };
+
+        // Add data to send with request????
+        const data = new FormData();
+        data.append('currency', currency);
+
+        // Send request
+        request.send(data);
+        return false;
+    };
 
     // Connect to websocket
     var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
@@ -12,8 +51,18 @@ document.addEventListener('DOMContentLoaded', () => {
     // After websocket is connected...
     socket.on('connect', () => {
 
-        // Register a new usern
-        document.querySelector('#')
+        // Register a new username
+        document.querySelector('#username-submit').onclick = () => {
+
+            // Generate a username from the input
+            const username = parseText(document.querySelector('#username-create').value);
+            socket.emit('registration', {'username': username});
+        };
+
+        // When a username has been created (or logged in), allow chatting feature
+        socket.on('logged in', data => {
+
+        })
 
         // After websocket is connected, create a channel.
         document.querySelector('#create-channel').onclick = () => {
