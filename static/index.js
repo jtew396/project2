fe9b3ae7582d60ee6acbe0212a4ac3a6b60bf14e
jtew396 <jtew396@gmail.com>
2019-10-13 12:42:15 -0500

    [Project2] Small changes.

--- static/index.js
+++ static/index.js
@@ -1,5 +1,5 @@
 // Template for creating a channel
-const template = Handlebars.compile("<li>New Channel</li>");
+// const template = Handlebars.compile("<li>New Channel</li>");
 
 // Storing a list of channels
 const channels = []
@@ -7,104 +7,47 @@ const channels = []
 // Storing a list of users
 const users = []
 
+// Storing the user's display name as username
+//const username = []
+
+// Once the DOM is loaded, run the following function
 document.addEventListener('DOMContentLoaded', () => {
 
-    // Hide the inner HTML of the frame before logging the user in
+    // Hide the inner HTML of the frame before loggin the user in
     document.querySelector('#frame').style.visibility = 'hidden';
 
     // This is adapted from the currency app, needs to be adjusted for register & login
     document.querySelector('#new-user').onsubmit = () => {
 
         // Initialize new request
-        const request = new XMLHttpRequest();
+        //const request = new XMLHttpRequest();
         const username = document.querySelector('#username-create').value;
-        request.open('POST', '/register');
-
+        document.querySelector('#frame').style.visibility = 'visible';
+        document.querySelector('#introduction').style.visibility = 'hidden';
+        //console.log(username);
+        //request.open('POST', '/register');
+        //console.log("Past the request.");
         // Callback function for when request completes
+        /*
         request.onload = () => {
-
+            console.log("Onload of the request.")
             // Extract JSON data from requests
             const data = JSON.parse(request.responseText);
+            console.log(data);
 
             // Update the webpage
             if (data.success) {
-                const contents = `${data.username}`;
+                const contents = `Hello ${data.username}!`;
+                document.querySelector('#introduction').style.visibility = 'hidden';
+                document.querySelector('#frame').style.visibility = 'visible';
                 document.querySelector('#result').innerHTML = contents;
             }
             else {
                 document.querySelector('#result').innerHTML = 'Username already exists.';
             }
         };
+        */
 
-        // Add data to send with request????
-        const data = new FormData();
-        data.append('currency', currency);
-
-        // Send request
-        request.send(data);
-        return false;
-    };
-
-    // Connect to websocket
-    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
-
-    // After websocket is connected...
-    socket.on('connect', () => {
-
-        // Register a new username
-        document.querySelector('#username-submit').onclick = () => {
-
-            // Generate a username from the input
-            const username = parseText(document.querySelector('#username-create').value);
-            socket.emit('registration', {'username': username});
-        };
-
-        // When a username has been created (or logged in), allow chatting feature
-        socket.on('logged in', data => {
-
-        })
-
-        // After websocket is connected, create a channel.
-        document.querySelector('#create-channel').onclick = () => {
-
-            // Generate a submitted channel.
-            const channel = parseText(document.querySelector('#new-channel').value);
-            channels.push(channel)
-        };
-    });
-
-    // On click for channel, send content to DOM.
-    document.querySelector('#create-channel').onclick = () => {
-
-        // Generate a submitted channel.
-        const channel = parseText(document.querySelector('#new-channel').value);
-        channels.push(channel)
-
-        // Add the channel to DOM
-        const channel_content = template({'value': roll});
-        document.querySelector('#channels').innerHTML += channel_content;
-    }
-
-    // Update text on popping state
-    window.onpopstate = e => {
-        const data = e.state;
-        document.title = data.title;
-        document.querySelector('#body').innerHTML = data.text;
-    };
-
-    // Renders contents of new page in main view.
-    function load_page(name) {
-        const request = new XMLHttpRequest();
-        request.open('GET', `/${name}`);
-        request.onload = () => {
-            const response = request.responseText;
-            document.querySelector('#body').innerHTML = response;
-
-            // Push state to URL.
-            document.title = name;
-            history.pushState({'title': name, 'text': response}, name, name);
-        };
-        request.send();
     };
 
 });
