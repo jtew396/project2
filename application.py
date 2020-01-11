05dfc18d17dacf16e88d1485151e113c33d91df9
jtew396 <jtew396@gmail.com>
2020-01-11 14:49:18 -0600

    [Project2] Working through display name creator.

--- application.py
+++ application.py
@@ -1,32 +1,110 @@
 import os
 
-from flask import Flask, jsonify, render_template, request
-from flask_socketio import SocketIO, emit
+from flask import Flask, jsonify, render_template, request, session
+from flask_session import Session
+from flask_socketio import SocketIO, send, emit
+from tempfile import mkdtemp
 
 # Configure application
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 socketio = SocketIO(app)
 
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
 
-@app.route("/")
+# Universal variables
+user = []
+users = []
+
+@socketio.on('message')
+
+@app.route("/", methods=["GET", "POST"])
 def index():
-    print("We made it to index.")
-    return render_template("index.html")
 
-@app.route("/register", methods=["POST"])
-def register():
-    print("We made it to register...")
-    # Query for the username
-    username = request.form.get("username-create")
-    print(username)
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
 
-    # Make sure the username was successfully passed
-    if not username:
-        return jsonify({"success": False})
+        # Ensure that a display name was submitted
+        # Need to write this condition
 
-    return jsonify({"success": True, "username": username})
+        # Gather the display_name string from the JS AJAX POST request
+        display_name = request.form['display_name']
 
+        # Create a user data dict to save to the users array
+        user_data = {
+            "id": users.length + 1,
+            "display_name": display_name
+        }
+
+        # Remember which user id corresponds to their display name
+        session["user_id"] = user_data["id"]
+
+        # Append the user data to the users array
+        users.append(user_data)
+
+    else:
+
+        print("We made it to index.")
+        return render_template("index.html", user=user)
+
+@socketio.on("message")
+def handleMessage(msg):
+    print('Message: ' + msg)
+    send(msg, broadcast=True)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Create a Display Name for the user"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("display_name"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+# @app.route("/register", methods=["POST"])
+# def register():
+#     print("We made it to register...")
+#     # Query for the username
+#     username = request.form.get("username-create")
+#     print(username)
+#
+#     # Make sure the username was successfully passed
+#     if not username:
+#         return jsonify({"success": False})
+#
+#     return jsonify({"success": True, "username": username})
+#
 # @socketio.on("send message")
 # def message(data)
 #     selection = data["selection"]
--- static/index.js
+++ static/index.js
@@ -7,36 +7,81 @@ const channels = []
 // Storing a list of users
 const users = []
 
-// Once the DOM is loaded, run the following function
-document.addEventListener('DOMContentLoaded', () => {
+function FlackSettings(users, channels) {
+	return settings
+};
 
-    // Hide the inner HTML of the frame before logging the user in
-    document.querySelector('#frame').style.visibility = 'hidden';
+$(document).ready(function() {
 
-    // Connect to web socket
-    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+	var socket = io.connect('http://127.0.0.1:5000');
 
-    // Once the web socket is established
-    socket.on('connect', () => {
+	$('#create_display_name').on('click', function() {
+		$.post("/", {
+			display_name: $('#display_name').val('')
+		});
+		console.log('Display Name Created');
+	});
+	$('#display_name').on('keypress', function(e) {
+		if(e.which == 13) {
+			$.post("/", {
+				display_name: $('#display_name').val('')
+			});
+			console.log('Display Name Created');
+		}
+	});
 
-        // Listen for the user to submit a display name
-        document.querySelector('#new-user').onclick = () => {
+	socket.on('connect', function() {
+		socket.send('User has connected!');
+	});
 
-            // Store the display name as username
-            const username = document.querySelector('#username-create').value;
+	socket.on('message', function(msg) {
+		$("#messages").append('<li>'+msg+'</li>');
+		console.log('Received message');
+	});
 
-            // Hide the welcome page and display name input
-            document.querySelector('#introduction').innerHTML = "";
-            document.querySelector('#introduction').classList.remove('p-5');
-
-            // Display the available chat rooms and users
-            document.querySelector('#frame').style.visibility = 'visible';
-
-        };
-
-        document.querySelector('#message-send').onclick = () => {
-
-            
-        };
+	$('#sendbutton').on('click', function() {
+		socket.send($('#myMessage').val());
+		$('#myMessage').val('');
+	});
+    $('#myMessage').on('keypress', function(e) {
+        if(e.which == 13) {
+            socket.send($('#myMessage').val());
+    		$('#myMessage').val('');
+        }
     });
+
 });
+
+// Once the DOM is loaded, run the following function
+//document.addEventListener('DOMContentLoaded', () => {
+//
+//    // Hide the inner HTML of the frame before logging the user in
+//    document.querySelector('#frame').style.visibility = 'hidden';
+//
+//    // Connect to web socket
+//    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
+//
+//    // Once the web socket is established
+//    socket.on('connect', () => {
+//
+//        // Listen for the user to submit a display name
+//        document.querySelector('#new-user').onclick = () => {
+//
+//            // Store the display name as username
+//            const username = document.querySelector('#username-create').value;
+//
+//            // Hide the welcome page and display name input
+//            document.querySelector('#introduction').innerHTML = "";
+//            document.querySelector('#introduction').classList.remove('p-5');
+//
+//            // Display the available chat rooms and users
+//            document.querySelector('#frame').style.visibility = 'visible';
+//
+//        };
+//
+//        document.querySelector('#message-send').onclick = () => {
+//
+//
+//        };
+//    });
+//});
